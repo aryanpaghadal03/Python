a10a426f857c843c3c26e71466965f0acf627ad1
Aryan_Paghadal <paghadalaryan3@gmail.com>
2025-09-06 10:58:02 +0530

    Add files via upload

---
+++ Bot/CR7.py
@@ -0,0 +1,76 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '8301512193:AAFLpTRGOaL4ynBPkmKKqDllWKsJSj08AtE'
+BOT_USERNAME: Final = '@CR7_bot'
+
+# commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello thanks for chatting with me! I am a CR7 fan")
+    
+    
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I am CR7 fan pls type something so I can help...")
+    
+    
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("This is a custom command...!")
+
+
+# responses
+def handle_responses(text: str) -> str:
+    processed: str = text.lower()
+    
+    if 'hello' in processed:
+        return 'Hey there!'
+    
+    if 'how are you' in processed:
+        return 'I am good!'
+    
+    if 'i love cr7' in processed:
+        return 'I am also!'
+    
+    return "I don't understand what you wrote...."
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f'User({update.message.chat.id}) in {message_type}: "{text}"')
+    
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_responses(new_text)
+        else:
+            return
+    else:
+        response: str = handle_responses(text)
+        
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+    
+
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+    
+    # commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    
+    # messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # errors
+    app.add_error_handler(error)
+    
+    # polls the bot
+    print("Polling.....")
+    app.run_polling(poll_interval=3)
